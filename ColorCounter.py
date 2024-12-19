d541994c3402601453e08c043d0fb36c3e52b7ec
Dror Maor <dror.m.maor@gmail.com>
2024-12-19 15:33:48 +0200

    Update ColorCounter.py
    removed "saving results" - not ready to cloud-sync it yet

--- ColorCounter.py
+++ ColorCounter.py
@@ -9,13 +9,18 @@ def ColorPrint(code):
 
 def main():
     # clear screen first
-    os.system('cls')
+    if os.name == "nt":
+        os.system('cls')
+    else:
+        os.system('clear')
+        
     size = 10
     ColorsDict = {31: 'Red', 32: 'Green', 33: 'Yellow', 34: 'Blue', 35: 'Magenta', 36: 'Cyan', 37: 'White'}
     ColorsList = list(ColorsDict.keys())
     matrix = [[0 for x in range(size)] for y in range(size)]
-    AllCurr = [[0 for x in range(size)] for y in range(size)]
-
+    blocks = [[0 for x in range(size)] for y in range(size)]
+    
+    # build the matrix
     for x in range(size):
         for y in range(size):
             matrix[x][y] = random.randint(ColorsList[0], ColorsList[len(ColorsList) - 1])
@@ -26,51 +31,46 @@ def main():
             ColorPrint(matrix[x][y])
         print()
 
-    # count the "dupes"
+    # count the "blocks"
     for x in range(size):
         for y in range(size):
             curr = matrix[x][y]
             # N
-            if y > 0 and matrix[x][y-1] == curr and AllCurr[x][y-1] == 0:
-                AllCurr[x][y-1] = 1
-                AllCurr[x][y] = 1
+            if y > 0 and matrix[x][y-1] == curr and blocks[x][y-1] == 0:
+                blocks[x][y-1] = 1
+                blocks[x][y] = 1
             # S
-            if y < size-1 and matrix[x][y+1] == curr and AllCurr[x][y+1] == 0:
-                AllCurr[x][y+1] = 1
-                AllCurr[x][y] = 1
+            if y < size-1 and matrix[x][y+1] == curr and blocks[x][y+1] == 0:
+                blocks[x][y+1] = 1
+                blocks[x][y] = 1
             # E
-            if x < size-1 and matrix[x+1][y] == curr and AllCurr[x+1][y] == 0:
-                AllCurr[x+1][y] = 1
-                AllCurr[x][y] = 1
+            if x < size-1 and matrix[x+1][y] == curr and blocks[x+1][y] == 0:
+                blocks[x+1][y] = 1
+                blocks[x][y] = 1
             # W
-            if x > 0 and matrix[x-1][y] == curr and AllCurr[x-1][y] == 0:
-                AllCurr[x-1][y] = 1
-                AllCurr[x][y] = 1
+            if x > 0 and matrix[x-1][y] == curr and blocks[x-1][y] == 0:
+                blocks[x-1][y] = 1
+                blocks[x][y] = 1
             
     matches = 0
     for x in range(size):
         for y in range(size):
-            matches += AllCurr[x][y]
+            matches += blocks[x][y]
 
     start_time = time.time()
-    guess = input("How many matches are there? ")
+    guess = input("How many color blocks are there? ")
     end_time = time.time()
     elapsed = round(end_time - start_time, 2)
     print (f"It took you {elapsed} seconds")
-    with open("results.txt", 'a') as file:
-        now = datetime.datetime.now()
-        # Time of Game	elapsed	guess	actual
-        text = "\n" + now.strftime("%m/%d/%Y %H:%M:%S") + "\t" + str(elapsed) + "\t" + str(guess) + "\t" + str(matches)
-        file.write(text)
     
     if int(guess) == matches:
-        print ("YOU ARE CORRECT !!!!")
+        print ("YOU ARE A GENIUS !!!!")
     else:
         print (f"Sorry, there were {matches}")
     # print the color matches
     for x in range(size):
         for y in range(size):
-            if AllCurr[x][y] == 1:
+            if blocks[x][y] == 1:
                 ColorPrint(matrix[x][y])
             else:
                 ColorPrint(30)
@@ -79,13 +79,7 @@ def main():
     if input("Play again? (Y/N) ").lower() == "y":
         main()
     else:
-        if input("Would you like to see your results? (Y/N) ").lower() == "y":
-            with open("results.txt", 'r') as file:
-                lines = file.read()
-                print (lines)
-                print()
         print ("Good bye")
         
 if __name__ == "__main__":
   main()
-
